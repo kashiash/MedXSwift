1a51ceb7788559927ac01d30e6ae0b4e3c10035f
Jacek Kosinski <kashiash@gmail.com>
2024-09-22 21:24:52 +0200

    Initial Commit

---
+++ MedX/MedXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedXApp.swift
+//  MedX
+//
+//  Created by Jacek Kosinski U on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MedXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MedXTests/MedXTests.swift
@@ -0,0 +1,16 @@
+//
+//  MedXTests.swift
+//  MedXTests
+//
+//  Created by Jacek Kosinski U on 22/09/2024.
+//
+
+import Testing
+
+struct MedXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
